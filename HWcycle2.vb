ed3e3dceab1ee2648a4fcd4217af11482840ed53
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-01-17 19:56:26 +0300

    Add files via upload

---
+++ HWcycle2.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+
+Module HWcycle
+    Sub Main(args As String())
+        Dim a, p, n, i As Long
+        Const max As Integer = 2147483647
+
+        i = 1
+        p = 1
+        Console.WriteLine(" Введите количество вводимых чисел ")
+        n = (Console.ReadLine())
+        For i = 1 To n Step 1
+            Console.WriteLine(" введите число ")
+            a = (Console.ReadLine())
+            p = p * a
+
+
+        Next
+        If (p > max / i) Then
+            Console.Write(" Память перегружина !!!")
+            Console.WriteLine(i - 1)
+
+        End If
+        Console.WriteLine(" Произведение N введенных Вами чисел равно ")
+        Console.WriteLine(p)
+
+
+
+
+    End Sub
+End Module
