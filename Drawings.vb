c3540b42011c6e84bddb8483af56ee9991435b5d
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-02-02 11:59:40 +0300

    Add files via upload

--- Drawings.vb
+++ Drawings.vb
@@ -5,77 +5,46 @@ Module Drawings
     Sub Main(args As String())
         Dim u As Integer
 
-
-        For u = 0 To 4
+        For u = 0 To 5
             Console.WriteLine(" Что ты хочешь, чтоб я нарисовал ")
             Console.WriteLine(" 1 = вертикальную линию,2 = горизонтальную,3 =  квадрат,4 =  цветной квадрат ?, 5 = самому нарисовать ")
             u = Console.ReadLine
+            Console.SetWindowSize(100, 50)
+            Select Case u
+                Case 1
 
-            If u = 1 Then
-                Console.SetWindowSize(100, 50)
+                    Vline(10, 5, 70)
+                Case 3
 
-                Vline(10, 5, 70)
+                    FilRect(10, 5, 70, 35)
 
-                Console.WriteLine()
-            End If
+                Case 2
 
-            If u = 3 Then
-                Console.SetWindowSize(100, 50)
+                    Hline(10, 5, 70)
 
-                FilRect(10, 5, 70, 35)
+                Case 4
 
-                Console.WriteLine()
+                    Nova(10, 5, 70, 35)
+                Case 6
 
-            End If
+                    line(10, 5, 70)
+                Case 5
 
-            If u = 2 Then
-                Console.SetWindowSize(100, 50)
-
-                Hline(10, 5, 70)
-
-                Console.WriteLine()
-
-            End If
-
-            If u = 4 Then
-                Console.SetWindowSize(100, 50)
-
-                Nova(10, 5, 70, 35)
-
-                Console.WriteLine()
-
-            End If
-
-            If u = 6 Then
-                Console.SetWindowSize(100, 50)
-
-                line(10, 5, 70)
-
-                Console.WriteLine()
-
-            End If
-
-            If u = 5 Then
-                Console.SetWindowSize(100, 50)
-
-                VIL()
-
-                Console.WriteLine()
-
-            End If
+                    VIL()
+            End Select
+            Console.WriteLine()
             Console.WriteLine(" спасибо за просмотр!!! ")
 
         Next
 
-
     End Sub
-
 
     Sub Hline(leftX As Byte, topY As Byte, length As Byte)
         Console.SetCursorPosition(leftX, topY)
         For i = 1 To length
             Console.Write(brush)
         Next
+
     End Sub
     Sub Vline(leftX As Byte, topY As Byte, length As Byte)
         Console.SetCursorPosition(leftX, topY)
@@ -83,12 +52,15 @@ Module Drawings
             Console.Write(brush)
             Console.SetCursorPosition(leftX, topY + i)
         Next
+
     End Sub
+
     Sub FilRect(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
         Console.SetCursorPosition(leftX, topY)
         For j = 0 To heigh - 1
             Hline(leftX, topY + j, width)
         Next
+
     End Sub
     Sub Nova(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
         Console.SetCursorPosition(leftX, topY)
@@ -96,13 +68,16 @@ Module Drawings
         For j = 0 To heigh - 1
             Hline(leftX, topY + j, width)
         Next
+
     End Sub
+
     Sub line(leftX As Byte, topY As Byte, length As Byte)
         Console.SetCursorPosition(leftX, topY)
         For i = 0 To length - 1
             Console.Write(brush)
             Console.SetCursorPosition(leftX, topY + i)
         Next
+
     End Sub
     Sub VIL()
         Dim leftx, topY, Length, heit As Byte
@@ -129,8 +104,7 @@ Module Drawings
             Next
             Console.SetCursorPosition(10, 3 + j)
         Next
+
     End Sub
-
-
 
 End Module
