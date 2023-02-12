39c6c548511aba97dcb693b7c7db6375e150d249
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-02-12 20:14:08 +0300

    Add files via upload

--- быки и коровы v2.vb
+++ быки и коровы v2.vb
@@ -5,30 +5,41 @@ Module Program
     Sub Main(args As String())
         Dim N, ver As String
         Dim start, ent As Integer
+        Dim bulls, cows As Integer
+
         'TODO:   Генерируем число
+        Console.ForegroundColor = ConsoleColor.Green
         Console.WriteLine("Введи диапозон")
         Console.WriteLine("Начало")
         start = Console.ReadLine
         Console.WriteLine("Конец")
         ent = Console.ReadLine
         N = Int((ent * Rnd()) + start)
+        Console.WriteLine(N)
         Console.WriteLine("Угадай число")
-        Console.Clear()
+
         Do
+
             ver = Console.ReadLine()
-            Console.WriteLine("Еще попытка")
-            For i = 0 To 3
-                If (N.Chars(i) = ver.Chars(0)) Then
-                    If i = 0 Then
-
-                        Console.WriteLine("Первая цифра вашей версии - БЫК")
-                    Else
-
-                        Console.WriteLine("Первая цифра вашей версии - Корова")
+            For j As Byte = 0 To 3
+                For i As Byte = 0 To 3
+                    If (N.Chars(i) = ver.Chars(j)) Then
+                        If i = j Then
+                            Console.WriteLine(j + 1)
+                            bulls += 1
+                        Else
+                            Console.WriteLine(j + 1)
+                            cows += 1
+                        End If
                     End If
-                End If
+                Next
             Next
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("have you found the cows = {0}", cows)
+            Console.WriteLine("have you found the bulls = {0}", bulls)
         Loop Until N = ver
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Victory")
 
     End Sub
 End Module
