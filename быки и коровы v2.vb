d1db44b6952a346c539fd4492dbf9fb7c216794c
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-02-20 09:55:50 +0300

    Add files via upload

--- быки и коровы v2.vb
+++ быки и коровы v2.vb
@@ -4,39 +4,39 @@ Imports System
 Module Program
     Sub Main(args As String())
         Dim N, ver As String
-        Dim start, ent As Integer
         Dim bulls, cows As Integer
-
         'TODO:   Генерируем число
         Console.ForegroundColor = ConsoleColor.Green
-        Console.WriteLine("Введи диапозон")
-        Console.WriteLine("Начало")
-        start = Console.ReadLine
-        Console.WriteLine("Конец")
-        ent = Console.ReadLine
-        N = Int((ent * Rnd()) + start)
+        Randomize()
+        N = Int((10000 * Rnd()) + 1000)
         Console.WriteLine(N)
         Console.WriteLine(CheckDublicates(N))
-
         Do
-
+            N = Int((10000 * Rnd()) + 1000)
+            Console.WriteLine(N)
+        Loop Until CheckDublicates(N) = False
+        Do
             ver = Console.ReadLine()
+            cows = 0
+            bulls = 0
             For j As Byte = 0 To 3
                 For i As Byte = 0 To 3
                     If (N.Chars(i) = ver.Chars(j)) Then
                         If i = j Then
-                            Console.WriteLine(j + 1)
+
                             bulls += 1
                         Else
-                            Console.WriteLine(j + 1)
+
                             cows += 1
                         End If
                     End If
+
                 Next
             Next
             Console.ForegroundColor = ConsoleColor.Red
             Console.WriteLine("have you found the cows = {0}", cows)
             Console.WriteLine("have you found the bulls = {0}", bulls)
+
         Loop Until N = ver
         Console.ForegroundColor = ConsoleColor.Green
         Console.WriteLine("Victory")
@@ -45,7 +45,7 @@ Module Program
     Function CheckDublicates(Num As String) As Boolean
         CheckDublicates = False
         For j = 0 To 2
-            For i = 1 To 3
+            For i = j + 1 To 3
                 If Num.Chars(j) = Num.Chars(i) Then
                     CheckDublicates = True
                 End If
