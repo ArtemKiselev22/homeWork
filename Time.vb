cfde253f6f65b590947edc127bc27c6570c01bad
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-03-05 18:13:38 +0300

    Add files via upload

--- Time.vb
+++ Time.vb
@@ -2,22 +2,20 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim sec, min, hour, min2, hor, min3, min4 As Integer
-        Dim ver As String
+        Dim ver, min, sekynd, min2, hour, ostatokmin, minut As Integer
         'Dim vrem As DateTime
         'Console.WriteLine("VREMY")
         'vrem = Console.ReadLine()
-
         Console.WriteLine("Enter the time in seconds")
-        sec = Console.ReadLine()
-        min = sec \ 60
+        ver = Console.ReadLine()
+        min = ver \ 60
         min2 = min * 60
-        hour = sec Mod 60
-        hor = min \ 60
-        min3 = hor * 60
-        min4 = min Mod 60
-        Console.WriteLine("sekynd = {0}", hour)
-        Console.WriteLine("Minut = {0}", min4)
-        Console.WriteLine("chas = {0}", hor)
+        sekynd = ver Mod 60
+        hour = min \ 60
+        ostatokmin = hour * 60
+        minut = min Mod 60
+        Console.WriteLine("sekynd = {0}", sekynd)
+        Console.WriteLine("Minut = {0}", minut)
+        Console.WriteLine("chas = {0}", hour)
     End Sub
 End Module
