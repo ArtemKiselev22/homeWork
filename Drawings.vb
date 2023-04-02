5a7f445ed5eefea06140ff55f5a3a180909e891b
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-04-02 22:09:02 +0300

    Add files via upload

--- Drawings.vb
+++ Drawings.vb
@@ -10,8 +10,8 @@ Module Drawings
         Dim key As ConsoleKeyInfo
         Console.ForegroundColor = ConsoleColor.DarkBlue
         Do
-            Console.WriteLine(" Что ты хочешь, чтоб я нарисовал ")
-            Console.WriteLine(" 1 = вертикальную линию,2 = горизонтальную,3 =  квадрат,4 =  цветной квадрат ?, 5 = самому нарисовать, 6 = сетка из квадратов, 7 = цветная сетка из квадратов,8 = орнамент из квадратиков, 9 = сделать пустой квадрат, 0 = линии, 10 = лестница, 11 = chess")
+            Console.WriteLine("Write a number and I'll draw it  ")
+            Console.WriteLine(" 1 = vertical line,2 = horizontal line,3 = square,4 =  colored square ?, 5 = draw it yourself, 6 = lots of squares, 7 = lots of colored squares,8 = ornament, 9 = empty square, 0 = lines in a row, 10 = Ladder, 11 = chess, 12 = Striped squares, 13 = TV")
             u = Console.ReadLine
             Console.SetWindowSize(100, 50)
             Select Case u
@@ -29,31 +29,37 @@ Module Drawings
                     Nova(10, 5, 70, 35)
 
                 Case 6
-                    line()
+                    line(10, 5, 70)
 
                 Case 5
                     VIL()
 
                 Case 7
-                    Jline()
+                    Jline(10, 5, 70)
 
                 Case 8
-                    fura()
+                    fura(10, 5, 5, 5)
 
                 Case 9
-                    square()
+                    square(10, 5, 10)
 
                 Case 0
-                    London()
+                    London(10, 5, 40)
 
                 Case 10
-                    stairs()
+                    stairs(4, 1, 2)
                 Case 11
-                    Chess()
+                    Chess(4, 7, 7, 7)
+                Case 12
+                    'Kvadro(10, 5, 10)
+                Case 13
+                    LineTV(10, 5, 70, 10)
+                Case 14
+                    NEWLineTV(4, 5, 70, 3)
 
             End Select
             Console.WriteLine()
-            Console.WriteLine(" хотите еще что-нибудь нарисовать ? y или n ")
+            Console.WriteLine(" Do you want something else? If yes - y if no - n")
 
             Do
                 key = Console.ReadKey(True)
@@ -98,62 +104,77 @@ Module Drawings
 
     End Sub
 
-    Sub line()
+    Sub Line(leftX As Byte, topY As Byte, length As Byte)
 
         For i = 1 To 8
-            Hline(2 + 10, 2 + i * 5, 70)
-            Vline(2 + 10 * i, 2 + 5, 37)
+            Hline(leftX, topY + i * 5, length)
+            Vline(leftX * i, topY + 5, length - 33)
         Next
 
     End Sub
-    Sub Jline()
+    Sub Jline(leftX As Byte, topY As Byte, length As Byte)
         Console.ForegroundColor = ConsoleColor.Green
         Console.BackgroundColor = ConsoleColor.Gray
         For i = 1 To 8
-            Hline(2 + 10, 2 + i * 5, 70)
-            Vline(2 + 10 * i, 2 + 5, 37)
+            Hline(leftX, topY + i * 5, length)
+            Vline(leftX * i, topY + 5, length - 33)
         Next
-
     End Sub
-    Sub fura()
+    Sub Fura(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
+
+        For j = 1 To heigh
+            For i = 1 To heigh
+                FilRect(leftX + 10 * i, topY + j * 6, width, heigh)
+            Next
+        Next
+    End Sub
+    Sub Fur(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
+
+        'For j = 1 To 8
+        '    For i = 1 To 8
+        '        FilRect(4 * i * 2, j * 7, 7, 7)
+        '    Next
+        'Next
 
         For j = 1 To 8
             For i = 1 To 8
-                FilRect(4 * i * 2, j * 7, 7, 7)
+                FilRect(leftX * i * 2, j * topY, width, heigh)
             Next
         Next
-
     End Sub
-    Sub square()
+    Sub Square(leftX As Byte, topY As Byte, length As Byte)
         Console.ForegroundColor = ConsoleColor.Yellow
         For j = 1 To 2
-            Hline(10, 5 * j, 10)
-            Vline(10 * j, 5, 7)
+            Hline(leftX, topY * j, 10)
+            Vline(leftX * j, topY, length - 3)
         Next
     End Sub
-    Sub ons()
+    Sub Ons()
         Console.ForegroundColor = ConsoleColor.Yellow
         Hline(7, 63, 65)
         Vline(7, 6, 59)
         Hline(7, 6, 65)
         Vline(71, 6, 59)
+
     End Sub
-    Sub London()
+    Sub London(leftX As Byte, topY As Byte, length As Byte)
+
         For i = 1 To 6
-            Hline(10, 5 * i, 40)
+            Hline(leftX, topY * i, length)
         Next
 
     End Sub
-    Sub stairs()
+    Sub Stairs(leftX As Byte, topY As Byte, length As Byte)
 
         For i = 1 To 46
-            Hline(4, i, 2 * i)
+            Hline(leftX, topY + i, length * i)
         Next
+
     End Sub
 
     Sub VIL()
         Dim leftx, topY, Length, heit As Byte
-        Const brux As Char = "0"c
+        'Const brux As Char = "0"c
         Dim f As Char
         Console.ForegroundColor = ConsoleColor.Red
         Console.Write("heit=")
@@ -164,7 +185,7 @@ Module Drawings
         topY = Console.ReadLine
         Console.Write("length=")
         Length = Console.ReadLine
-        Console.Write("Какой буквой будем писать ?")
+        Console.Write("What symbol should I draw ?")
         f = Console.ReadLine
         Console.SetCursorPosition(10, 60)
         Console.SetWindowSize(100, 50)
@@ -178,17 +199,60 @@ Module Drawings
             Console.SetCursorPosition(10, 3 + j)
         Next
     End Sub
-    Sub Chess()
+    Sub Chess(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
         Console.ForegroundColor = ConsoleColor.White
-        fura()
+
+        fur(4, 7, 7, 7)
         For j = 1 To 8
             For i = 1 To 8
                 If (i + j) Mod 2 = 1 Then
                     Console.ForegroundColor = ConsoleColor.Red
-                    FilRect(4 * i * 2, j * 7, 7, 7)
+                    FilRect(leftX * i * 2, j * topY, width, heigh)
                 End If
             Next
         Next
         ons()
+    End Sub
+    'Sub Kvadro(leftX As Byte, topY As Byte, length As Byte)
+    '    For i = 1 To 4
+    '        Square(10 * i, 5 * i, 10 * i)
+    '    Next
+    'End Sub
+
+    Sub LineTV(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
+        For j = 1 To heigh
+            For i = 1 To heigh - 5
+                Console.BackgroundColor = ConsoleColor.White
+                Console.ForegroundColor = ConsoleColor.White
+                Hline(leftX, topY * i + j, width)
+            Next
+        Next
+        For j = 1 To heigh - 6
+            For i = 1 To heigh - 3
+                Console.BackgroundColor = ConsoleColor.Red
+                Console.ForegroundColor = ConsoleColor.Red
+                Hline(leftX, topY * i + j, width)
+            Next
+        Next
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.ForegroundColor = ConsoleColor.DarkBlue
+    End Sub
+
+    Sub NEWLineTV(leftX As Byte, topY As Byte, width As Byte, heigh As Byte)
+        Console.BackgroundColor = ConsoleColor.Red
+        Console.ForegroundColor = ConsoleColor.Red
+        FilRect(4, 5, 70, 28)
+
+        For j = 1 To 5
+            For i = 1 To 5
+                If (i + j) Mod 3 = 1 Then
+                    Console.BackgroundColor = ConsoleColor.White
+                    Console.ForegroundColor = ConsoleColor.White
+                    FilRect(leftX, topY * i + j, width, heigh)
+                End If
+            Next
+        Next
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.ForegroundColor = ConsoleColor.DarkBlue
     End Sub
 End Module
