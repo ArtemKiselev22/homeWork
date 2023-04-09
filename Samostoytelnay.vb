8a5b4e16ea5a0c12beed1a2ba57e29606af4ba74
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-04-09 15:58:44 +0300

    Add files via upload

--- Samostoytelnay.vb
+++ Samostoytelnay.vb
@@ -4,32 +4,24 @@ Imports System.Text.Json
 Module Program
     Sub Main(args As String())
         Dim Naim As String
-        Dim num As Integer
+        Dim u As Integer
         Dim Mas As Double
-        Dim key As ConsoleKeyInfo
         Console.ForegroundColor = ConsoleColor.Red
-        Do
+        Console.WriteLine("Сколько всего товаров вы добавите ?")
+        u = Console.ReadLine()
+        For i = 1 To u
             Console.WriteLine("Добавте товар")
             Console.Write("Наименование  ")
             Naim = Console.ReadLine()
             Console.Write("Масса  ")
             Mas = Console.ReadLine()
-            num = num + 1
             Console.ForegroundColor = ConsoleColor.Green
             Console.WriteLine("Ваш отчет")
             Console.WriteLine("Наименование = {0} ", Naim)
             Console.WriteLine("Масса = {0} ", Mas)
-            Console.WriteLine("Номер ячейки = {0}", num)
+            Console.WriteLine("Номер ячейки = {0}", i)
             Console.ForegroundColor = ConsoleColor.Red
-            Console.WriteLine("add a product ?")
-            Console.WriteLine("Yes = y , Not = n")
-            Do
-                key = Console.ReadKey(True)
-            Loop Until key.KeyChar = "y"c Or key.KeyChar = "n"c
-            Console.WriteLine(key.KeyChar)
-            Console.Clear()
-        Loop Until key.KeyChar = "n"c
-
+        Next
 
     End Sub
 End Module
