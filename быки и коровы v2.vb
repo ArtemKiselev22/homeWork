b87e0c0369584e428b3f06775af19398282efb9e
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-04-10 08:12:00 +0300

    Add files via upload

--- быки и коровы v2.vb
+++ быки и коровы v2.vb
@@ -8,20 +8,21 @@ Module Program
         Dim key As ConsoleKeyInfo
         Dim moves As Integer
         Console.WriteLine("Нажмите на любую кнопку")
-        'Here we make a random of numbers
         Randomize()
         Do
             Console.ReadKey(True)
             Console.ForegroundColor = ConsoleColor.Green
-            '17 And 19 checking for duplicate randomness
+            '19 checking for duplicate randomness
             Do
+                'Here we make a random of numbers
                 N = Int((9000 * Rnd()) + 1000)
             Loop Until CheckDublicates(N) = False
             Console.WriteLine(N)
             Do
-                '23-27 Is user input And checking for duplicates
+                '23-27 Is user input And checking for duplicates and by length
                 ver = Console.ReadLine()
                 moves += 1
+                'Correct Input
                 If Not IsCorrect(ver) Then
                     Continue Do
                 End If
@@ -32,21 +33,16 @@ Module Program
                     For i As Byte = 0 To 3
                         If (N.Chars(i) = ver.Chars(j)) Then
                             If i = j Then
-
                                 bulls += 1
                             Else
-
                                 cows += 1
                             End If
                         End If
-
                     Next
                 Next
-
                 Console.ForegroundColor = ConsoleColor.Red
                 Console.WriteLine("have you found the cows = {0}", cows)
                 Console.WriteLine("have you found the bulls = {0}", bulls)
-
             Loop Until N = ver
             'Repeat the game And how many moves were made
             Console.ForegroundColor = ConsoleColor.Green
@@ -59,7 +55,6 @@ Module Program
             Console.WriteLine(key.KeyChar)
             Console.Clear()
         Loop Until key.KeyChar = "n"c
-
     End Sub
     'Function for duplicates
     Function CheckDublicates(Num As String) As Boolean
@@ -71,16 +66,13 @@ Module Program
                 End If
             Next
         Next
-
     End Function
     'The function for duplicates from the user
     Function IsCorrect(Num As String) As Boolean
         If Not Num.Length = 4 Then Return False
-
         For i = 0 To 3
             If Not Char.IsDigit(Num.Chars(i)) Then Return False
         Next
-
         Return Not CheckDublicates(Num)
     End Function
 End Module
