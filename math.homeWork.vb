b059d480112b86d7bb8b5641fdde8e96bca455fe
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-04-12 22:25:21 +0300

    Add files via upload

---
+++ math.homeWork.vb
@@ -0,0 +1,17 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim a, b, z, x, c, abc, y As Double
+        a = 1.5
+        b = 0.7
+        c = 2
+        abc = 1.5 * 0.7 * 2
+        x = MathF.Pow(MathF.E, a - MathF.Cos(b)) / MathF.Sqrt(MathF.Abs(abc))
+        Console.WriteLine(x)
+        y = MathF.Log(a) - b * c
+        Console.WriteLine(y)
+        z = MathF.Sin(x * y) + 2
+        Console.WriteLine(z)
+    End Sub
+End Module
