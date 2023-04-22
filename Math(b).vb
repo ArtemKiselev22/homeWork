86146e5ca76e8ef3688e997caf628f1519719ab9
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-04-23 00:19:47 +0300

    Add files via upload

--- Math(b).vb
+++ Math(b).vb
@@ -2,14 +2,16 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim x, y, z As Double
-        MathX()
-        Math5()
+        Dim x As Double
+        x = 2.2
+        MathX(x)
+        x = -5.3
+        MathX(x)
+
     End Sub
 
-    Function MathX()
-        Dim x, y, z As Double
-        x = 2.2
+    Function MathX(x As Double)
+        Dim y, z As Double
         If x > 2 Then
             y = x * Math.Log(Math.Abs(x))
         Else
@@ -18,18 +20,5 @@ Module Program
         Console.WriteLine("y1 = {0}", y)
         z = 2 * Math.Pow(Math.Cos(Math.Pow(y, 3)), 2) - Math.Log(Math.Abs(y))
         Console.WriteLine("z1 = {0}", z)
-    End Function
-
-    Function Math5()
-        Dim x, y, z As Double
-        x = -5.3
-        If x > 2 Then
-            y = x * Math.Log(Math.Abs(x))
-        Else
-            y = Math.Pow(x, 2)
-        End If
-        Console.WriteLine("y2 = {0}", y)
-        z = 2 * Math.Pow(Math.Cos(Math.Pow(y, 3)), 2) - Math.Log(Math.Abs(y))
-        Console.WriteLine("z2 = {0}", z)
     End Function
 End Module
